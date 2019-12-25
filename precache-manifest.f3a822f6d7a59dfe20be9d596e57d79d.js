658c6ea3dd90fb44c945f4d1e277eaa0883332e9
ViridianaAvem <viridiana.avem@gmail.com>
2019-12-25 00:19:44 -0600

    Updates

---
+++ precache-manifest.f3a822f6d7a59dfe20be9d596e57d79d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96d2b5a4b77795fcc77dcd7e13d24b91",
+    "url": "/MoviesApp/index.html"
+  },
+  {
+    "revision": "e264ebc0b767267d8e03",
+    "url": "/MoviesApp/static/css/2.777dbe41.chunk.css"
+  },
+  {
+    "revision": "6cf080bcdb62c6668f7a",
+    "url": "/MoviesApp/static/css/main.bfcf5167.chunk.css"
+  },
+  {
+    "revision": "e264ebc0b767267d8e03",
+    "url": "/MoviesApp/static/js/2.4da7ef58.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/MoviesApp/static/js/2.4da7ef58.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6cf080bcdb62c6668f7a",
+    "url": "/MoviesApp/static/js/main.5c11960b.chunk.js"
+  },
+  {
+    "revision": "c488ae22832000664b69",
+    "url": "/MoviesApp/static/js/runtime-main.8817e795.js"
+  }
+]);
